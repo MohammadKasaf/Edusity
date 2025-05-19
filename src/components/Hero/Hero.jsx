4f939ddaa9b8ebb6bf36c6080792666d46d8ef93
Mohammad Kasaf Siddique <147969017+MohammadKasaf@users.noreply.github.com>
2025-05-19 11:26:09 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,14 @@
-import dark_arrow from '../../assets/dark-arrow.png';
+import white_arrow from '../../assets/white-arrow.png';
 import './Hero.css';
 
 const Hero = () => {
+
   return (
  <div className="hero container">
   <div className="hero-text">
     <h1>We Ensure better education for a better world</h1>
     <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
-    <button className="btn">Explore more <img src={dark_arrow} alt="arrow" /></button>
+    <button className="btn">Explore more <img src={white_arrow} alt="arrow" /></button>
   </div>
 </div>
 
